a9c2499efeaae1278285635ad3d588d71fe57d19
pjsmith404 <pjsmith404@gmail.com>
2025-02-10 20:40:45 +1100

    add block_to_block_type function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,14 @@
+import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = []
 
@@ -8,3 +19,48 @@ def markdown_to_blocks(markdown):
             blocks.append(line.strip()) 
 
     return blocks
+
+def block_to_block_type(block):
+    if re.match(r"^#{1,6} ", block):
+        return BlockType.HEADING
+    elif (block[:3] == "```" and block[-3:] == "```"):
+        return BlockType.CODE
+    elif is_quote_block(block):
+        return BlockType.QUOTE
+    elif is_unordered_list(block):
+        return BlockType.UNORDERED_LIST
+    elif is_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
+
+def is_quote_block(block):
+    lines = block.split("\n")
+    for line in lines:
+        if line[:2] != "> ":
+            return False
+
+    return True
+
+def is_unordered_list(block):
+    lines = block.split("\n")
+    for line in lines:
+        if line[:2] != "* " and line[:2] != "- ":
+            return False
+
+    return True
+
+def is_ordered_list(block):
+    line_numbers = re.findall(r"(\d). ", block)
+
+    if len(line_numbers) <= 0:
+        return False
+
+    if int(line_numbers[0]) != 1:
+        return False
+
+    if all(line_numbers[i] < line_numbers[i + 1] for i in range(len(line_numbers) - 1)):
+        return True
+
+    return False
+
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import (
+    BlockType,
+    markdown_to_blocks,
+    block_to_block_type,
+)
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -49,6 +53,101 @@ It has some **bold** and *italic* words inside of it.
             ),
         ]
         self.assertEqual(markdown_to_blocks(markdown), result)
+
+    def test_block_to_block_type_paragraph(self):
+        block = "This is a paragraph of text"
+        result = BlockType.PARAGRAPH
+        self.assertEqual(block_to_block_type(block), result)
+
+    def test_block_to_block_type_heading(self):
+        blocks = [
+            "# Heading",
+            "## Heading 2",
+            "### Heading 3",
+            "#### Heading 4",
+            "##### Heading 5",
+            "###### Heading 6",
+        ]
+        result = BlockType.HEADING
+        for block in blocks:
+            self.assertEqual(block_to_block_type(block), result)
+
+    def test_block_to_block_type_heading_invalid(self):
+        blocks = [
+            "Head# ing",
+            "Heading 2 ## ",
+            "###Heading 3",
+        ]
+        result = BlockType.PARAGRAPH
+        for block in blocks:
+            self.assertEqual(block_to_block_type(block), result)
+
+    def test_block_to_block_type_code(self):
+        block = """```
+This is a block of code
+On multiple lines
+```"""
+        result = BlockType.CODE
+        self.assertEqual(block_to_block_type(block), result)
+
+    def test_block_to_block_type_code_invalid(self):
+        block = """```
+This is an invalid block of code
+On multiple lines"""
+        result = BlockType.PARAGRAPH
+        self.assertEqual(block_to_block_type(block), result)
+
+    def test_block_to_block_type_quote(self):
+        block = """> This is a multi line
+> block quote"""
+        result = BlockType.QUOTE
+        self.assertEqual(block_to_block_type(block), result)
+
+    def test_block_to_block_type_quote_invalid(self):
+        invalid_block = """> This is not a valid
+block quote"""
+        result = BlockType.PARAGRAPH
+        self.assertEqual(block_to_block_type(invalid_block), result)
+
+    def test_block_to_block_type_unordered_list(self):
+        blocks = [
+            "* This is an\n* unordered list",
+            "- This is an\n- unordered list",
+            "* This is an\n- unordered list",
+        ]
+        result = BlockType.UNORDERED_LIST
+        for block in blocks:
+            self.assertEqual(block_to_block_type(block), result)
+
+    def test_block_to_block_type_unordered_list_invalid(self):
+        blocks = [
+            "* This is an invalid\nunordered list",
+            "- This is an invalid\n-unordered list",
+        ]
+        result = BlockType.PARAGRAPH
+        for block in blocks:
+            self.assertEqual(block_to_block_type(block), result)
+
+    def test_block_to_block_type_ordered_list(self):
+        block = """1. This is
+2. an ordered
+3. list"""
+        result = BlockType.ORDERED_LIST
+        self.assertEqual(block_to_block_type(block), result)
+
+    def test_block_to_block_type_ordered_list_invalid(self):
+        block = """1. This is
+3. an invalid ordered
+2. list"""
+        result = BlockType.PARAGRAPH
+        self.assertEqual(block_to_block_type(block), result)
+
+    def test_block_to_block_type_ordered_list_invalid(self):
+        block = """3. This is
+3. an invalid ordered
+2. list"""
+        result = BlockType.PARAGRAPH
+        self.assertEqual(block_to_block_type(block), result)
 
 if __name__ == "__main__":
     unittest.main()
